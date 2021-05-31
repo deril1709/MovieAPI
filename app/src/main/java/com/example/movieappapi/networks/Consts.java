8fa399021cfa3fd91a5bd561f94f0f3d5e7686cc
deril1709 <punktator17@gmail.com>
2021-05-31 18:17:27 +0800

    done

---
+++ app/src/main/java/com/example/movieappapi/networks/Consts.java
@@ -0,0 +1,12 @@
+package com.example.movieappapi.networks;
+
+public class Consts {
+
+    public static final String BASEURL = "https://api.themoviedb.org/3/";
+    public static final String APIKEY = "65f67805aa494353d114815d6a69bbe6";
+    public static final String LANGUAGE = "en-US";
+    public static final String PAGE = "1";
+    public static final String IMAGEBASEURL = "https://image.tmdb.org/t/p/w500/";
+    public static final String APIERROR = "DEBUG:API ERROR";
+
+}
